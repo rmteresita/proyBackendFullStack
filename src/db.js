87c6f3f2420074e4ff0114bb449aa95ea276c017
rmteresita <g12rmteresita@gmail.com>
2023-01-12 08:54:02 -0600

    sec version

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
 import {createPool} from 'mysql2/promise'
+import {
+    DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD
+} from './config.js'
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'imshawol24',
-    port: 3306,
-    database: 'farmacia'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_DATABASE
 
 })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import productosRoutes from './routes/productos.routes.js'
 import clientesRoutes from './routes/clientes.routers.js'
 import empleadosRoutes from './routes/empleados.routers.js'
 import indexRoutes from './routes/index.routes.js'
+import {PORT} from './config.js'
 
 const app = express()
 
@@ -19,5 +20,5 @@ app.use((req, res, next) => {
     })
 })
 
-app.listen(3002)
-console.log('Server running on port 3002')
+app.listen(PORT)
+console.log('Server running on port', PORT)
